6d347e843080203c865f7ba2613adf343686f2df
Steven Williams <steven.m.williams@yandex.com>
2021-11-12 13:17:24 -0500

    Change day 1 solution to print answer

--- 2015/day1/haskell_1/app/Main.hs
+++ 2015/day1/haskell_1/app/Main.hs
@@ -16,4 +16,9 @@ basementStep :: String -> Int
 basementStep = length . takeWhile (>=0) . scanl (\acc x -> acc + floorDirection x) 0
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  s <- readFile "../input"
+  let answer1 = floorNumber s
+      answer2 = basementStep s
+  putStrLn $ "Part 1: " ++ show answer1
+  putStrLn $ "Part 2: " ++ show answer2
